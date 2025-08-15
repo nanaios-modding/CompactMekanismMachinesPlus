3e9c99ed474d86581a3fb5033a3416fc00131616
nanaios <game.ryd.131@gmail.com>
2025-08-15 22:40:38 +0900

    ブロックが正常に追加されていることを確認

--- src/main/java/com/nanaios/CompactMekanismMachinesPlus/common/CompactMekanismMachinesPlus.java
+++ src/main/java/com/nanaios/CompactMekanismMachinesPlus/common/CompactMekanismMachinesPlus.java
@@ -2,7 +2,9 @@ package com.nanaios.CompactMekanismMachinesPlus.common;
 
 import com.nanaios.CompactMekanismMachinesPlus.common.registries.CompactPlusBlocks;
 import com.nanaios.CompactMekanismMachinesPlus.common.registries.CompactPlusContainerTypes;
+import com.nanaios.CompactMekanismMachinesPlus.common.registries.CompactPlusCreativeTabs;
 import com.nanaios.CompactMekanismMachinesPlus.common.registries.CompactPlusTileEntityTypes;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
@@ -17,8 +19,14 @@ public class CompactMekanismMachinesPlus {
 
         //eventBusに登録
         CompactPlusBlocks.BLOCKS.register(modEventBus);
+        CompactPlusCreativeTabs.CREATIVE_TABS.register(modEventBus);
         CompactPlusContainerTypes.CONTAINER_TYPES.register(modEventBus);
         CompactPlusTileEntityTypes.TILE_ENTITY_TYPES.register(modEventBus);
 
+    }
+
+    @SuppressWarnings("removal")
+    public static ResourceLocation rl(String path) {
+        return new ResourceLocation(CompactMekanismMachinesPlus.MODID, path);
     }
 }
---
+++ src/main/java/com/nanaios/CompactMekanismMachinesPlus/common/CompactPlusLang.java
@@ -0,0 +1,23 @@
+package com.nanaios.CompactMekanismMachinesPlus.common;
+
+import mekanism.api.text.ILangEntry;
+import net.minecraft.Util;
+
+public enum CompactPlusLang implements ILangEntry {
+    COMPACTMEKANISMMACHINESPLUS("other","mod_name");
+
+    private final String key;
+
+    CompactPlusLang(String type, String path) {
+        this(Util.makeDescriptionId(type, CompactMekanismMachinesPlus.rl(path)));
+    }
+
+    CompactPlusLang(String key) {
+        this.key = key;
+    }
+
+    @Override
+    public String getTranslationKey() {
+        return key;
+    }
+}
