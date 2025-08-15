4efc63e6502a80564e798aaa2bf1d1f17eaff135
nanaios <game.ryd.131@gmail.com>
2025-08-15 21:21:10 +0900

    依存を追加

---
+++ src/main/java/com/nanaios/CompactMekanismMachinesPlus/CompactMekanismMachinesPlus.java
@@ -0,0 +1,8 @@
+package com.nanaios.CompactMekanismMachinesPlus;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(CompactMekanismMachinesPlus.MODID)
+public class CompactMekanismMachinesPlus {
+    public static final String MODID = "compactmekanismmachinesplus";
+}
