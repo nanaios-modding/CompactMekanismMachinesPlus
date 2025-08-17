dd79e0fafe49502275ff8cca9e72b2114ece7fb0
nanaios <game.ryd.131@gmail.com>
2025-08-17 16:54:49 +0900

    mixinはまたあとでやろう

--- src/main/java/com/nanaios/CompactMekanismMachinesPlus/mixin/MixinDataType.java
+++ src/main/java/com/nanaios/CompactMekanismMachinesPlus/mixin/MixinDataType.java
@@ -1,11 +1,9 @@
 package com.nanaios.CompactMekanismMachinesPlus.mixin;
 
-import com.nanaios.CompactMekanismMachinesPlus.common.CompactPlusLang;
 import mekanism.api.text.EnumColor;
 import mekanism.api.text.ILangEntry;
 import mekanism.common.tile.component.config.DataType;
 import org.spongepowered.asm.mixin.*;
-import org.spongepowered.asm.mixin.gen.Invoker;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -13,28 +11,27 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import java.util.ArrayList;
 import java.util.Arrays;
 
+//TODO なんとかしろ、未来の私
+
 @Mixin(value = DataType.class, remap = false)
 public class MixinDataType {
     @Shadow
     @Final
     @Mutable
-    public static DataType[] $VALUES;
-
-    @Invoker("<init>")
-    private DataType mixinDataType$invokeInit(String langEntry, int color, ILangEntry par3, EnumColor par4, CallbackInfo ci) {
-        throw new AssertionError();
-    }
+    private static DataType[] $VALUES;
 
     //@Unique
     //public static final MixinDataType INPUT_3 = mixinDataType$addVariant(CompactPlusLang.SIDE_DATA_INPUT_3,EnumColor.PINK);
 
     //INPUT_3(MekanismLang.SIDE_DATA_INPUT_2, EnumColor.ORANGE);
 
-    @Inject(method = "<clinit>", at = @At("RETURN"))
-    public static void mixinDataType$clinit(CallbackInfo ci) {
+    @Inject(method = "<init>", at = @At("RETURN"))
+    private void mixinDataType$init(CallbackInfo ci) {
         ArrayList<DataType> variants = new ArrayList<DataType>(Arrays.asList(MixinDataType.$VALUES));
 
-        variants.add(mixinDataType$create(CompactPlusLang.SIDE_DATA_INPUT_3,EnumColor.PINK));
+        System.out.println("mixin GO!!");
+        System.out.println($VALUES.length);
+        //variants.add();
 
         $VALUES = variants.toArray($VALUES);
         return;
