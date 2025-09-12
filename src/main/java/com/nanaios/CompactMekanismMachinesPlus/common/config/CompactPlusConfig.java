24e6062b5fc12e34dbd7ce3e5ef28ccf730eb8a1
nanaios <game.ryd.131@gmail.com>
2025-09-12 23:43:34 +0900

    上げ忘れかな

---
+++ src/main/java/com/nanaios/CompactMekanismMachinesPlus/common/config/CompactPlusConfig.java
@@ -0,0 +1,14 @@
+package com.nanaios.CompactMekanismMachinesPlus.common.config;
+
+import net.minecraftforge.fml.ModContainer;
+import net.minecraftforge.fml.ModLoadingContext;
+
+public class CompactPlusConfig {
+    private CompactPlusConfig() {
+    }
+
+    @SuppressWarnings("removal")
+    public static void registerConfigs(ModLoadingContext modLoadingContext) {
+        ModContainer modContainer = modLoadingContext.getActiveContainer();
+    }
+}
