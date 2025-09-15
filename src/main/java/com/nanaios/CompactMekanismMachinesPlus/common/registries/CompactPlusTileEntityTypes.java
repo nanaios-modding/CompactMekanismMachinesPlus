9d5c3b3e0384b0a7b3e476b20f3b42ab61981550
nanaios <game.ryd.131@gmail.com>
2025-09-15 19:14:13 +0900

    おそらく修正

--- src/main/java/com/nanaios/CompactMekanismMachinesPlus/common/registries/CompactPlusTileEntityTypes.java
+++ src/main/java/com/nanaios/CompactMekanismMachinesPlus/common/registries/CompactPlusTileEntityTypes.java
@@ -4,10 +4,10 @@ import com.nanaios.CompactMekanismMachinesPlus.common.CompactMekanismMachinesPlu
 import com.nanaios.CompactMekanismMachinesPlus.common.tile.TileEntityCompactFusionReactor;
 import com.nanaios.CompactMekanismMachinesPlus.common.tile.TileEntityCompactSPS;
 import com.nanaios.CompactMekanismMachinesPlus.common.tile.TileEntityCompactThermoelectricBoiler;
+import mekanism.common.capabilities.Capabilities;
 import mekanism.common.registration.impl.TileEntityTypeDeferredRegister;
 import mekanism.common.registration.impl.TileEntityTypeRegistryObject;
-
-//TODO 1.21.1対応
+import mekanism.common.tile.base.TileEntityMekanism;
 
 public class CompactPlusTileEntityTypes {
     private CompactPlusTileEntityTypes() {}
@@ -18,8 +18,23 @@ public class CompactPlusTileEntityTypes {
     public static final TileEntityTypeRegistryObject<TileEntityCompactThermoelectricBoiler> COMPACT_THERMOELECTRIC_BOILER;
 
     static {
-        COMPACT_FISSION_REACTOR = TILE_ENTITY_TYPES.register(CompactPlusBlocks.COMPACT_FUSION_REACTOR, TileEntityCompactFusionReactor::new);
-        COMPACT_SPS = TILE_ENTITY_TYPES.register(CompactPlusBlocks.COMPACT_SPS,TileEntityCompactSPS::new);
-        COMPACT_THERMOELECTRIC_BOILER = TILE_ENTITY_TYPES.register(CompactPlusBlocks.COMPACT_THERMOELECTRIC_BOILER, TileEntityCompactThermoelectricBoiler::new);
+        COMPACT_FISSION_REACTOR = TILE_ENTITY_TYPES.mekBuilder(CompactPlusBlocks.COMPACT_FUSION_REACTOR,TileEntityCompactFusionReactor::new)
+                .clientTicker(TileEntityMekanism::tickClient)
+                .serverTicker(TileEntityMekanism::tickServer)
+                .withSimple(Capabilities.CONFIG_CARD)
+                .withSimple(Capabilities.EVAPORATION_SOLAR)
+                .build();
+        COMPACT_SPS = TILE_ENTITY_TYPES.mekBuilder(CompactPlusBlocks.COMPACT_SPS,TileEntityCompactSPS::new)
+                .clientTicker(TileEntityMekanism::tickClient)
+                .serverTicker(TileEntityMekanism::tickServer)
+                .withSimple(Capabilities.CONFIG_CARD)
+                .withSimple(Capabilities.EVAPORATION_SOLAR)
+                .build();
+        COMPACT_THERMOELECTRIC_BOILER = TILE_ENTITY_TYPES.mekBuilder(CompactPlusBlocks.COMPACT_THERMOELECTRIC_BOILER, TileEntityCompactThermoelectricBoiler::new)
+                .clientTicker(TileEntityMekanism::tickClient)
+                .serverTicker(TileEntityMekanism::tickServer)
+                .withSimple(Capabilities.CONFIG_CARD)
+                .withSimple(Capabilities.EVAPORATION_SOLAR)
+                .build();
     }
 }
