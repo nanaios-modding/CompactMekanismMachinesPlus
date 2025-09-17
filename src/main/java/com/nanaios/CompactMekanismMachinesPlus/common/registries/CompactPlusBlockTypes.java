06c180d43f01de776fb81ed7406fb7547bc182b2
nanaios <game.ryd.131@gmail.com>
2025-09-17 11:50:07 +0900

    起動だけはできた

--- src/main/java/com/nanaios/CompactMekanismMachinesPlus/common/registries/CompactPlusBlockTypes.java
+++ src/main/java/com/nanaios/CompactMekanismMachinesPlus/common/registries/CompactPlusBlockTypes.java
@@ -7,6 +7,7 @@ import mekanism.api.Upgrade;
 import mekanism.common.MekanismLang;
 import mekanism.common.config.MekanismConfig;
 import mekanism.common.content.blocktype.Machine;
+import mekanism.common.lib.transmitter.TransmissionType;
 import mekanism.common.registries.MekanismSounds;
 import mekanism.generators.common.GeneratorsLang;
 import mekanism.generators.common.config.MekanismGeneratorsConfig;
@@ -25,17 +26,20 @@ public class CompactPlusBlockTypes {
                 .withEnergyConfig(MekanismGeneratorsConfig.generators.fusionEnergyCapacity::get)
                 .withGui(() -> CompactPlusContainerTypes.COMPACT_FUSION_REACTOR, GeneratorsLang.FUSION_REACTOR)
                 .withSound(GeneratorsSounds.FUSION_REACTOR)
+                .withSideConfig(TransmissionType.CHEMICAL,TransmissionType.FLUID,TransmissionType.ITEM,TransmissionType.ENERGY)
                 .withSupportedUpgrades(Upgrade.MUFFLING)
                 .build();
         COMPACT_SPS = Machine.MachineBuilder.createMachine(() -> CompactPlusTileEntityTypes.COMPACT_SPS, MekanismLang.DESCRIPTION_SPS_CASING)
                 .withGui(() -> CompactPlusContainerTypes.COMPACT_SPS,MekanismLang.SPS)
                 .withSound(MekanismSounds.SPS)
                 .withEnergyConfig(MekanismConfig.storage.spsPort)
+                .withSideConfig(TransmissionType.CHEMICAL,TransmissionType.ENERGY)
                 .withSupportedUpgrades(Upgrade.MUFFLING)
                 .build();
         COMPACT_THERMOELECTRIC_BOILER = Machine.MachineBuilder.createMachine(() -> CompactPlusTileEntityTypes.COMPACT_THERMOELECTRIC_BOILER,MekanismLang.DESCRIPTION_BOILER_CASING)
                 .withGui(() -> CompactPlusContainerTypes.COMPACT_THERMOELECTRIC_BOILER,MekanismLang.BOILER)
                 .withSupportedUpgrades(Upgrade.MUFFLING)
+                .withSideConfig(TransmissionType.CHEMICAL,TransmissionType.FLUID,TransmissionType.HEAT)
                 .build();
     }
 }
